371851279c5ec7179b60306997b850e42c7e0637
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-18 21:58:31 +0200

    2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
---crete database and user and his privileges
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE User IF NOT EXISTS 'hbnb_dev'@'localhost' identified by 'hbnb_dev_pwd';
-grand all privileges on 'hbnb_dev_db'.* to 'hbnb_dev'@'localhost';
-grand select on 'performance_schema'.* to 'hbnb_dev'@'localhost';
-flush privileges;
-
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
