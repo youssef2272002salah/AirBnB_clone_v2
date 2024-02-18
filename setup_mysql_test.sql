f66421320910c90d5f24ef0ba73f3b3707e020da
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-18 22:04:20 +0200

    1

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
---creat database for test
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* to 'hbnb_test'@'localhost';
-GRANT SELECT on 'performance_schema' to 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
