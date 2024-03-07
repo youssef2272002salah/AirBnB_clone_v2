ed6da713a7457480836c05bc85474d444cee53aa
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-07 20:20:18 +0200

    3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,36 @@
 #!/usr/bin/python3
-""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
-    ..an archive to your web servers, using the function do_deploy: """
-
+# fabric script to send the archive file to my servers
 
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
 
-
-env.hosts = ['100.25.142.157', '54.160.93.135']  # <IP web-01>, <IP web-02>
-# ^ All remote commands must be executed on your both web servers
-# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+env.hosts = ['100.25.142.157', '54.160.93.135']
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
+    '''pycodestyle'''
     if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # so now filename is <web_static_2021041409349.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    # curr = '/data/web_static/current'
-    tmp = "/tmp/" + filename
+        return False
 
+    archive_file = archive_path[9:]
+    re_archive = '/data/web_static/releases/{}'.format(archive_file[:-4])
+# print(archive_file)    web_static_20240307150135.tgz
+# print(re_archive)/data/web_static/releases/web_static_20240307150135
     try:
-        put(archive_path, "/tmp/")
-        # ^ Upload the archive to the /tmp/ directory of the web server
-        run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        # ^ Delete the archive from the web server
-        run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
+        # upload
+        put(archive_path, '/tmp/')
+        # make dir
+        run('sudo mkdir -p {}'.format(re_archive))
+        # extract
+        run('sudo tar -xzf /tmp/{} -C {}'.format(archive_file, re_archive))
+        # remove the uploaded file
+        run('sudo rm /tmp/{}'.format(archive_file))
+
+        run('sudo mv {}/web_static/* {}'.format(re_archive, re_archive))
+        run('sudo rm -rf /data/web_static/releases/web_static')
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(re_archive))
         return True
     except BaseException:
         return False
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python3
 """Fabric script module"""
 from fabric.api import *
-import time
-import os
+from datetime import datetime
+from os.path import exists
 
 env.hosts = ['100.25.142.157', '54.160.93.135']  # <IP web-01>, <IP web-02>
 env.user = 'ubuntu'
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static"""
-    try:
-        local("sudo mkdir -p versions")
-        archive_path = 'versions/web_static_{}.tgz'.format(
-                time.strftime('%Y%m%d%H%M%S'))
-        results = local('sudo tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except Exception:
+    """fuck pycode style """
+# make the dir to store the .tgz files
+    local("sudo mkdir -p versions")
+# make the name of the file tha alx wanted
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+# tar : command to make the archive
+# -c :create a new archive
+# -v :display the progress while archiving.
+# -z :compress using gzip
+# -f : specif the name of file
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+
+    if result.succeeded:
+        return filename
+    else:
         return None
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to my web servers"""
-    if not os.path.exists(archive_path):
+    '''pycodestyle'''
+    if exists(archive_path) is False:
         return False
 
     archive_file = archive_path[9:]
-    release_version = '/data/web_static/releases/{}'.format(archive_file[:-4])
+    re_archive = '/data/web_static/releases/{}'.format(archive_file[:-4])
+# print(archive_file)    web_static_20240307150135.tgz
+# print(re_archive)/data/web_static/releases/web_static_20240307150135
+    try:
+        # upload
+        put(archive_path, '/tmp/')
+        # make dir
+        run('sudo mkdir -p {}'.format(re_archive))
+        # extract
+        run('sudo tar -xzf /tmp/{} -C {}'.format(archive_file, re_archive))
+        # remove the uploaded file
+        run('sudo rm /tmp/{}'.format(archive_file))
 
-    # archive_file: web_static_20231210153350.tgz
-    # release_version: /data/web_static/releases/web_static_20231210153350
-
-    put(archive_path, '/tmp/')
-    run('sudo mkdir -p {}'.format(release_version))
-    run('sudo tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
-    run('sudo rm /tmp/{}'.format(archive_file))
-    run('sudo mv {}/web_static/* {}'.format(release_version, release_version))
-    run('sudo rm -rf /data/web_static/releases/web_static')
-    run('sudo rm -rf /data/web_static/current')
-    run('sudo ln -s {} /data/web_static/current'.format(release_version))
-
-    print('New version deployed!')
-    return True
+        run('sudo mv {}/web_static/* {}'.format(re_archive, re_archive))
+        run('sudo rm -rf /data/web_static/releases/web_static')
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(re_archive))
+        return True
+    except BaseException:
+        return False
 
 
 def deploy():
