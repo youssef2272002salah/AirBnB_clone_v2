8f94c2e9d51c85b8bdeb05d6d7bfc0752cdf5e56
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-07 15:07:21 +0200

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,16 @@ from datetime import datetime
 
 def do_pack():
     """fuck pycode style """
+# make the dir to store the .tgz files
     local("sudo mkdir -p versions")
-
+# make the name of the file tha alx wanted
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
-
+# tar : command to make the archive
+# -c :create a new archive
+# -v :display the progress while archiving.
+# -z :compress using gzip
+# -f : specif the name of file
     result = local("sudo tar -cvzf {} web_static".format(filename))
 
     if result.succeeded:
