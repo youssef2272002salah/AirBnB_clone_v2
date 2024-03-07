b67fca8a6191f30f6fd26e4bb969696cb5193d7d
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-07 14:55:34 +0200

    pycode

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-#make a function that generate a .tgz archive of th content of web_static folders 
+# make a function that generate a .tgz archive of th content of web_static
+# folders
 
 from fabric.api import *
 from datetime import datetime
 
+
 def do_pack():
     """fabric script to mke a make a dir that contain a .tgz archive of the web_static"""
-    local ("sudo mkdir -p versions")
+    local("sudo mkdir -p versions")
 
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
-
 
     result = local("sudo tar -czvf {filename} web_static")
 
@@ -18,5 +19,3 @@ def do_pack():
         return filename
     else:
         return None
-
-
