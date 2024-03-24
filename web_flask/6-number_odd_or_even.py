0af25db5df628198e3cf2740bddf861492416b87
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-24 23:46:39 +0200

    6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,57 +1,65 @@
 #!/usr/bin/python3
-'''display “C ” followed by the value of the text 
+'''display “C ” followed by the value of the text
 variable (replace underscore _ symbols with a space )'''
 
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-   return 'HBNB'
+    """ Returns some text. """
+    return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    text= text.replace('_',' ')
+    """ Returns some text. """
+    text = text.replace('_', ' ')
     return f'C {text}'
-
 
 
 @app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text= "is_cool"):
-    text= text.replace('_',' ')
+def python_text(text="is_cool"):
+    """ Returns some text. """
+    text = text.replace('_', ' ')
     return f'Python {text}'
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def python_number(n):
-    n =str(n)
+    """ Returns some text. """
+    n = str(n)
     return f'{n} is a number'
-    
+
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def python_number_template(n):
-    n =str(n)
+    """ Returns some text. """
+    n = str(n)
     message = f'Number: {n}'
-    return render_template('5-number.html', message= message)
+    return render_template('5-number.html', message=message)
+
 
 @app.route('/number_odd_or_even/<int:n>')
 def python_number_odd_even(n):
-        n = int(n)  # Convert n to an integer cause it is string
-        if n % 2 == 0:
-            message = f'Number: {n} is even'
-        else:
-            message = f'Number: {n} is odd'
-        return render_template('6-number_odd_or_even.html', message=message)
-    
-if __name__== '__main__':
-    app.run(host='0.0.0.0',port='5000')
+    """ Returns some text. """
+    n = int(n)  # Convert n to an integer cause it is string
+    if n % 2 == 0:
+        message = f'Number: {n} is even'
+    else:
+        message = f'Number: {n} is odd'
+    return render_template('6-number_odd_or_even.html', message=message)
 
 
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
