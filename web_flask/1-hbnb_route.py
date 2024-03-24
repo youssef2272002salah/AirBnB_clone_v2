f31a435c00d056a10aa38fd5000fccd192a1ba8e
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-24 23:28:56 +0200

    2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,17 +2,20 @@
 """a script that starts a Flask web application """
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-   return 'HBNB'
+    """ Returns some text"""
+    return 'HBNB'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000')
-    
+    app.run(host='0.0.0.0', port='5000')
